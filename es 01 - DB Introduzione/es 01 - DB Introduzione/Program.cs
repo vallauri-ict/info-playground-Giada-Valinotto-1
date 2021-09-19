e241ec5d39d9c330408248d406fc07f933db254d
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-09-19 14:35:34 +0200

    Es 01 - Introduzione
    Caricamento di un esercizio di introduzione ai database e ai comandi base di sql

---
+++ es 01 - DB Introduzione/es 01 - DB Introduzione/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace es_01___DB_Introduzione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
