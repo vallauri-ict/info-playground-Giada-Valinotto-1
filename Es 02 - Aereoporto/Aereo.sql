f5cf8367db00526c56cf48d72ec7a42e20da25e7
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-09-24 16:17:23 +0200

    Es 02 - Update

--- Es 02 - Aereoporto/Aereo.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Aereo]
-(
-	[tipoAereo] VARCHAR(20) NOT NULL PRIMARY KEY, 
-    [numPasseggeri] INT NOT NULL, 
-    [qtaMerci] INT NOT NULL
-)
--- Es 02 - Aereoporto/Aeroporto.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Aeroporto]
-(
-	[citta] VARCHAR(50) NOT NULL PRIMARY KEY, 
-    [nazione] VARCHAR(50) NOT NULL, 
-    [numPiste] INT NOT NULL
-)
