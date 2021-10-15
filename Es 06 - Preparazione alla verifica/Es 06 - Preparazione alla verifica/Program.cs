4d0f87408234c6a9b4881cc01811677ab3927594
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-10-15 13:50:32 +0200

    Esercitazione pre-verifica

---
+++ Es 06 - Preparazione alla verifica/Es 06 - Preparazione alla verifica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es_06___Preparazione_alla_verifica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
