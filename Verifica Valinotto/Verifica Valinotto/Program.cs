ded5fc5a41c556813619b27b040f3175216d8902
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-11-04 11:56:35 +0100

    Caricamento della verifica

---
+++ Verifica Valinotto/Verifica Valinotto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Verifica_Valinotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
