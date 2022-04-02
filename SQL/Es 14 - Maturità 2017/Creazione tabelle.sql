daa1fe2e154922af49651f4d2a11c97b17dbccae
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2022-04-02 21:03:44 +0200

    Svolgimento maturità 2017

---
+++ SQL/Es 14 - Maturità 2017/Creazione tabelle.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE Passeggeri(
+Id_Utente int primary key identity(1,1),
+nome varchar(20) not null,
+cognome varchar(20) not null,
+telefono varchar(10) not null,
+email varchar(40) not null,
+docIdentita varchar(10) not null
+)
+
+CREATE TABLE Autisti(
+Id_Utente int primary key identity(1,1),
+nome varchar(20) not null,
+cognome varchar(20) not null,
+telefono varchar(10) not null,
+email varchar(40) not null,
+fotografia varchar(10) not null,
+datiAuto varchar(50) not null,
+scadPatente date not null
+)
+
+CREATE TABLE Viaggi(
+Id_Viaggio int primary key identity(1,1),
+cittaPartenza varchar(25) not null,
+cittaArrivo varchar(25) not null,
+tempiStimati int not null,
+prezzo decimal(3,2) not null,
+costo decimal(3,2) not null,
+[data] date not null,
+ora time not null,
+soste int not null,
+bagagli bit not null,
+animali bit not null,
+prenotabile bit not null,
+Id_Autista int foreign key references Autisti(id_Utente) not null
+)
+
+CREATE TABLE Prenotazioni(
+Id_Viaggio int foreign key references Viaggi(Id_Viaggio) not null,
+Id_Utente  int foreign key references Passeggeri(Id_Utente) not null,
+stato bit not null, 
+primary key(Id_Viaggio, Id_Utente)
+)
+
+CREATE TABLE FeedbackAutista(
+Id_FeedbackAutista int primary key not null,
+voto int not null,
+descrizione varchar(100),
+Id_Viaggio int foreign key references Viaggi(Id_Viaggio) not null,
+Id_Utente int foreign key references Passeggeri(Id_Utente) not null
+)
+
+CREATE TABLE FeedbackPasseggero(
+Id_FeedbackPasseggero int primary key not null,
+voto int not null,
+descrizione varchar(100),
+Id_Viaggio int foreign key references Viaggi(Id_Viaggio) not null,
+Id_Autista int foreign key references Autisti(Id_Utente) not null
+)
---
+++ SQL/Es 14 - Maturità 2017/Query.sql
@@ -0,0 +1,12 @@
+﻿SELECT a.nome, v.costo, a.datiAuto, v.ora
+FROM Viaggi v, Autisti a
+WHERE v.Id_Autista = a.Id_Utente and p.Id_Viaggio = v.Id_Viaggio and v.cittaArrivo='Carmagnola' and v.cittaPartenza = 'Torino' and v.data = '2022-04-02' and p.stato=1
+ORDER BY v.ora;
+
+SELECT pas.email, pas.cognome, pas.nome
+FROM Prenotazioni p, Passeggeri pas
+WHERE p.stato = 1 and p.Id_Utente= pas.Id_Utente
+
+SELECT *
+FROM Passeggeri p, FeedbackAutista fa, Viaggi v, Prenotazioni pren
+WHERE p.Id_Utente = fa.Id_Utente and v.Id_Viaggio = pren.Id_Viaggio and v.Id_Viaggio= 1 and 1<(SELECT AVG(fa.voto) FROM FeedbackAutista fa, Prenotazioni p WHERE fa.Id_Utente= p.Id_Utente)
